33560ce7076800a020c8251e586cb88045b9b44e
Gabriel Fray <gfraycastilho@gmail.com>
2025-05-06 00:10:14 -0300

    Solved task

--- app/main.py
+++ app/main.py
@@ -1,19 +1,20 @@
+from typing import List
+
 class Car:
-    def __init__(self, comfort_class, clean_mark, brand):
+    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
 
-
 class CarWashStation:
-    def __init__(self, distance_from_city_center, clean_power, average_rating, count_of_ratings):
+    def __init__(self, distance_from_city_center: float, clean_power: int,
+                 average_rating: float, count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-
-    def serve_cars(self, cars:list):
+    def serve_cars(self, cars: List[Car]) -> float:
         total_income = 0
         for car in cars:
             if car.clean_mark < self.clean_power:
@@ -22,20 +23,16 @@ class CarWashStation:
 
         return round(total_income, 1)
 
+    def calculate_washing_price(self, car: Car) -> float:
+        price = (car.comfort_class * (self.clean_power - car.clean_mark) *
+                 self.average_rating / self.distance_from_city_center)
+        return round(price, 1)
 
-    def calculate_washing_price(self, car):
-            price = (car.comfort_class * (self.clean_power - car.clean_mark) *
-            self.average_rating / self.distance_from_city_center)
-
-            return round(price, 1)
-
-
-    def wash_single_car(self, car):
+    def wash_single_car(self, car: Car) -> None:
         if car.clean_mark < self.clean_power:
             car.clean_mark = self.clean_power
 
-
-    def rate_service(self, rating):
+    def rate_service(self, rating: int) -> None:
         total_ratings = self.count_of_ratings + 1
         self.average_rating = round(((self.average_rating * self.count_of_ratings) + rating) / total_ratings, 1)
         self.count_of_ratings = total_ratings
